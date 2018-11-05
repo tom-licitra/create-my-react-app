8792634f3a6b2e285c61c9f32a4fb883b145b061
Tom Licitra <tom.licitra@gmail.com>
2018-11-05 18:54:27 -0500

    template complete

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
+
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send('Internal Server Error');
+})
+
+const PORT = process.env.PORT || 8080;
+const init = () => {
+  app.listen( PORT, console.log(`App listening on port ${PORT}`));
+}
+
+init();
